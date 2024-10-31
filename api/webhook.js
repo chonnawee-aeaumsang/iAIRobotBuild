a441ba2796d65ce8bb5c7aa84b59d9318c14e804
chonnawee-aeaumsang <110365562+chonnawee-aeaumsang@users.noreply.github.com>
2024-10-31 16:00:29 +0700

    reDomain

--- api/webhook.js
+++ api/webhook.js
@@ -2,7 +2,7 @@ const TelegramBot = require("node-telegram-bot-api");
 
 const TOKEN = "7498251188:AAGYxE1L2aGuTXx-VdjQHZn9UQRSK6svJmw";
 const gameName = "iAIGame"; // Replace with your game's short name
-const gameUrl = "https://i-ai-robot-build.vercel.app/"; // Your game URL
+const gameUrl = "https://i-ai-robot.vercel.app/"; // Your game URL
 const imageUrl = "https://imgur.com/ZGgcA9c"
 
 const bot = new TelegramBot(TOKEN, { polling: false });
